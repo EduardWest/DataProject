41bec97c9fffa35e11719ddbdbde85e4e3a42acb
EduardWest <60728891+EduardWest@users.noreply.github.com>
2020-09-03 01:24:36 +0300

    Update Прогнозирование цены на недвижимость.py

--- Прогнозирование цены на недвижимость.py
+++ Прогнозирование цены на недвижимость.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 # Импортируем все необходимые библиотеки
 import numpy as np 
 import pandas as pd 
@@ -21,31 +15,16 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 
 
-# In[4]:
-
-
 # Смотрим на первоначальные данные
 df = pd.read_csv('house_sales.csv')
 df.head()
 
-
-# In[3]:
-
-
 df.info() #Проверяем тип данных
-
-
-# In[9]:
-
 
 frame = df.iloc[:,2:6] #строим диаграмму рассеяния для некоторых переменных
 scatter_matrix(frame, figsize=(16, 9), diagonal="kde") 
 #scatter_matrix(frame, figsize=(16, 9), diagonal="hist") 
 plt.show()
-
-
-# In[11]:
-
 
 # Как можно увидеть - целевая переменная имеет лог-нормальное распределение
 Y = df['price']
@@ -54,55 +33,25 @@ for i in range(len(df.columns)):
     print(df.columns[i])
     plt.scatter(df[df.columns[i]], Y, c='red', marker = '.')
     plt.show()
-
-
-# 
-
-# In[6]:
-
-
+    
 f, axes = plt.subplots(1, 2,figsize=(16,9)) #Строим ящик с усами
 sns.boxplot(x=df['bedrooms'],y=df['price'], ax=axes[0])
 sns.boxplot(x=df['floors'],y=df['price'], ax=axes[1])
 
-
-# In[12]:
-
-
 df.isnull().any() #Проверка на нулевые значения
-
-
-# In[13]:
-
 
 # Удаляем поле 'id'
 df.drop("id", axis = 1, inplace = True)
 df.describe()
 
-
-# In[14]:
-
-
 df['floors'].value_counts()
-
-
-# In[37]:
-
 
 correlation = df.corr()
 fig=plt.figure(figsize=(16, 16))
 heatmap = sns.heatmap(correlation, annot=True)
 fig.savefig('correlation.png')
 
-
-# In[11]:
-
-
 df.corr()['price'].sort_values()
-
-
-# In[15]:
-
 
 #Гистограмма распределения
 df1=df[[
@@ -116,44 +65,19 @@ sns.despine(left=True, bottom=True)
 [x.title for x in h.ravel()];
 [x.yaxis.tick_left() for x in h.ravel()];
 
-
-# In[16]:
-
-
 f = plt.subplots(figsize=(15,9))
 sns.boxplot(x=df['floors'],y=df['price'])
-
-
-# In[14]:
-
 
 f = plt.subplots(figsize=(15,9))
 sns.boxplot(x=df['bedrooms'],y=df['price'])
 # Для 8 комнатных домов самый большой разброс в цене
 
 
-# In[17]:
-
-
 f = plt.subplots()
 sns.boxplot(x=df['waterfront'], y=df['price'])
 
-
-# In[18]:
-
-
 f = plt.subplots(figsize=(15,9))
 sns.boxplot(x=df['grade'],y=df['price'])
-
-
-# model
-# buldings
-# 
-
-# 
-
-# In[19]:
-
 
 features =["long","sqft_lot15","yr_renovated" ,"yr_built",
            "condition","sqft_lot","floors", "waterfront",
@@ -170,11 +94,6 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random
 
 print("number of test samples :", x_test.shape[0])
 print("number of training samples:",x_train.shape[0])
-
-
-# In[24]:
-
-
 # L1 регрессия
 for i in np.arange(0, 1, 0.1):
     RidgeModel = Ridge(alpha = i, fit_intercept = True,
@@ -186,10 +105,6 @@ for i in np.arange(0, 1, 0.1):
     accuracy = RidgeModel.score(x_test, y_test)
     print('Accuracy:{}, alpha:{}'.format(accuracy, round(i, 1)))
 
-
-# In[25]:
-
-
 for i in np.arange(0,1.1,0.1):
     lasso = Lasso(alpha = i, fit_intercept = True, normalize = False,
                   max_iter = 1000, tol = 0.0001, selection = 'cyclic')
@@ -200,10 +115,6 @@ for i in np.arange(0,1.1,0.1):
     accuracy = lasso.score(x_test, y_test)
     print('Accuracy:{}, alpha:{}'.format(accuracy, round(i, 1)))
 
-
-# In[33]:
-
-
 # Линейная регрессия
 lr = LinearRegression(fit_intercept = True, normalize=True,
                       copy_X = True, n_jobs = 8)
@@ -213,10 +124,6 @@ print(cross_val_score(lr, x_train, y_train, cv = 3))
 pred = lr.predict(x_test)
 accuracy = lr.score(x_test, y_test)
 print('predict data ', accuracy) 
-
-
-# In[34]:
-
 
 # L1 + L2 Регрессия
 for i in np.arange(0, 1.1, 0.1):
@@ -230,10 +137,6 @@ for i in np.arange(0, 1.1, 0.1):
     accuracy = en.score(x_test, y_test)
     print('Accuracy:{}, alpha:{}'.format(accuracy, round(i, 1)) 
 
-
-# In[38]:
-
-
 # Метод ближайших соседей
 for i in np.arange(1, 21, 1):
     knn = KNeighborsRegressor(n_neighbors = i, weights = 'uniform',
@@ -245,10 +148,6 @@ for i in np.arange(1, 21, 1):
     accuracy = knn.score(x_test, y_test)
     print('Accuracy:{},k = {}'.format(accuracy, i)) 
 
-
-# In[37]:
-
-
 # Дерево решений
 for i in range(1, 15):
     dtr = DecisionTreeRegressor(criterion = 'mse', splitter = 'best',
@@ -259,10 +158,6 @@ for i in range(1, 15):
     pred = dtr.predict(x_test)
     accuracy = dtr.score(x_test, y_test)
     print('Accuracy: {}, depth:{}'.format(accuracy, i))
-
-
-# In[39]:
-
 
 # Градиентный бустинг
 for i in np.arange(0.05, 0.4, 0.05):
@@ -276,95 +171,44 @@ for i in np.arange(0.05, 0.4, 0.05):
     accuracy = gbr.score(x_test, y_test)
     print('Accuracy:{}, learning rate:{}'.format(accuracy, i))
 
-
-# best
-
-# In[40]:
-
-
 # Обработка данных
 df1 = df
 df1['sqft_living']/df1['floors']
-
-
-# In[41]:
-
 
 df1['sqft_living/floors'] = df1['sqft_living']/df1['floors']
 df1.head()
 
 
-# In[42]:
-
-
 df1.drop("sqft_living", axis = 1, inplace = True)
-
-
-# In[43]:
-
 
 df1.drop("floors", axis = 1, inplace = True)
 
-
-# In[44]:
-
-
 df1.head()
 
-
-# In[45]:
 
 
 df1["coordinate"] = df1["long"] + df1["lat"]
 df1.head()
 
-
-# In[46]:
-
-
 df1.drop("lat", axis = 1, inplace = True)
 df1.drop("long", axis = 1, inplace = True)
 df1.head()
 
-
-# In[47]:
-
-
 df1["yr_renovated"].value_counts()
 
-
-# In[48]:
-
-
 len(df1["yr_renovated"])
-
-
-# In[ ]:
-
 
 df1["yr_renovated"]
 
 
-# In[ ]:
-
-
 df1["yr_renovated"][1]='0'
-
-
-# In[ ]:
 
 
 df1.drop("yr_renovated", axis = 1, inplace = True)
 df1
 
 
-# In[49]:
-
-
 df1.columns
-
-
-# In[51]:
 
 
 features =['bedrooms', 'bathrooms', 'sqft_lot', 'waterfront','view', 'condition', 
@@ -379,9 +223,6 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.30, rand
 
 print("number of test samples :", x_test.shape[0])
 print("number of training samples:", x_train.shape[0])
-
-
-# In[52]:
 
 
 # L1 регрессия
@@ -453,21 +294,10 @@ for i in np.arange(1, 21, 1):
     accuracy = knn.score(x_test, y_test)
     print('Accuracy:{}, k = {}'.format(accuracy, i))
     print()
-
-
-# In[74]:
-
-
 plt.plot(pred ** 2.81812)
-
-
-# In[75]:
-
 
 plt.plot(y_test ** 2.81812)
 
-
-# In[80]:
 
 
 # Дерево решений
@@ -482,10 +312,6 @@ for i in range(1, 15):
     print('Accuracy: {}, depth:{}'.format(accuracy, i))
     print(sep = ' ')
 
-
-# In[81]:
-
-
 # Градиентный бустинг
 for i in np.arange(0.05, 0.4, 0.05):
     gbr = GradientBoostingRegressor(loss = 'ls', learning_rate = i, n_estimators = 100,
@@ -497,9 +323,6 @@ for i in np.arange(0.05, 0.4, 0.05):
     pred = gbr.predict(x_test)
     accuracy = gbr.score(x_test, y_test)
     print('Accuracy:{}, learning rate:{}'.format(accuracy, i))
-
-
-# In[ ]:
 
 
 
